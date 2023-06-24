3f89abf79fa6d2c1def38f9efad685760792f581
Marina Vasques Rodrigues <131878241+marinavasqr23@users.noreply.github.com>
2023-06-24 20:23:45 -0300

    Update page.tsx

--- app/listphone/page.tsx
+++ app/listphone/page.tsx
@@ -20,12 +20,12 @@ export default async function Phone (){
     return(
         
     
-        <div className="bg-black LOJA">
+        <div className="bg-black LOJA" key={phone.nome}>
         
         
         {response.map((phone) => (
         
-        <div className=" Produtos flex flex-row justify-around p-10 align-center" key={phone.nome}>
+        <div className=" Produtos flex flex-row justify-around p-10 align-center">
             <a className="Img m-30"><img src={phone.img} alt={phone.nome} width={150} height={150}/></a> 
              <p  className="flex flex-row text-white m-30 pt-10 font-normal">{phone.nome}</p> 
              <p className="m-30 pt-10 font-sm">{phone.descricao}</p>
